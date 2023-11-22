92302c2abc2e21994f4256db986c71525a569ced
Карпин Кирилл <kirill_karpin@hotmail.com>
2023-11-22 22:37:02 +0500

    feature: extend base entity

--- Entities/Abstractions/BaseEntity.cs
+++ Entities/Abstractions/BaseEntity.cs
@@ -3,4 +3,10 @@
 public class BaseEntity
 {
     public Guid Id { get; set; }
+    public int Sort { get; set; }
+    public bool Active { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public Guid CreatedBy { get; set; }
+    public Guid UpdatedBy { get; set; }
 }

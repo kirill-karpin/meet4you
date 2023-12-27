aba1dd5a3982ed3bc4a97488c0d7e3707b017740
Pavel <p.skryabin2007@gmail.com>
2023-12-27 17:34:53 +0300

    Добавлена таблица UserLocation

--- DataAccess/DataContext.cs
+++ DataAccess/DataContext.cs
@@ -13,4 +13,5 @@ public class DataContext : DbContext
     public DbSet<User.User> Users { get; set; }
     public DbSet<Location.Country.Country> Countries { get; set; }
     public DbSet<Location.City.City> Cities { get; set; }
+    public DbSet<Location.UserLocation> UserLocations { get; set; }
 }

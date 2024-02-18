47929d2817fa253267d8385d13f695fde26ae52c
Карпин Кирилл <kirill_karpin@hotmail.com>
2024-02-18 21:55:32 +0500

    feature: impl status online

---
+++ WebApp/Hubs/StatusHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApp.Hubs;
+
+
+
+public static class UserHandler
+{
+    public static HashSet<string> ConnectedIds = new HashSet<string>();
+}
+
+//TODO ADD ONLINE STATUS SERVICE
+
+public class StatusHub : Hub
+{
+    public async Task FetchStatus()
+    {
+        var count = UserHandler.ConnectedIds.Count();
+        
+        var status = new AppStatus() {
+            OnlineCount = count
+        };
+        
+        Console.WriteLine("FetchStatus");
+        
+        await Clients.All.SendAsync("ReceiveStatus", status);
+    }
+    
+    public override Task OnConnectedAsync()
+    {
+        UserHandler.ConnectedIds.Add(Context.ConnectionId);
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+
+        return base.OnDisconnectedAsync(exception);
+    }
+}
+
+public class AppStatus
+{
+    public int OnlineCount { set; get; } = 0;
+}

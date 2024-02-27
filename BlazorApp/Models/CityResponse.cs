e0286df77e17ebd5f400504cc4a354c7a64afa1c
Pavel <p.skryabin2007@gmail.com>
2024-02-27 17:25:28 +0300

    add location service

---
+++ BlazorApp/Models/CityResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Models
+{
+    public class CityResponse
+    {
+        public Guid Id { get; set; }
+        public Guid CountryId { get; set; }
+        public string Name { get; set; }
+    }
+}

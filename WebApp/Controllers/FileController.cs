0b4935bb2b1041a278551339bf427c6de4a1c276
Карпин Кирилл <kirill_karpin@hotmail.com>
2024-04-03 22:53:46 +0500

    adapter

---
+++ WebApp/Controllers/FileController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+/*
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("/api/files")]
+
+interface IFileAdapter
+{
+    public Task<string> SaveFile()
+    {
+        
+    }
+    
+    public Task<byte> GetFileById(id)
+    {
+        
+    }
+    
+    public Task<byte> RemoveFile(id)
+    {
+        
+    }
+}
+
+public class FileStorageAdpater {
+
+}
+
+public interface IFileService
+{
+    public Task<string> SaveFile()
+    {
+        
+    }
+    
+    public Task<byte> GetFileById(id)
+    {
+        
+    }
+    
+    public Task<byte> RemoveFile(id)
+    {
+        
+    }
+}
+
+class FileService : IFileService
+{
+    private readonly IFileAdapter _fileAdapter;
+
+    public FileService(IFileAdapter fileAdapter)
+    {
+        _fileAdapter = fileAdapter;
+    }
+}
+
+
+
+
+public class FileController : Controller
+{
+    private readonly IFileService _fileService;
+
+    public FileController(IFileService fileService)
+    {
+        _fileService = fileService;
+    }
+
+    public Task<string> SaveFile(Byte data)
+    {
+        _fileService.SaveFile(data);
+    }
+    
+    public Task<byte> GetFileById(id)
+    {
+        _fileService.GetFileById(id);
+    }
+    
+    public Task<byte> RemoveFile(id)
+    {
+        _fileService.RemoveFile(id);
+    }
+}*/
